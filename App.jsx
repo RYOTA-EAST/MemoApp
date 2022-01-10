efe5d1741c2daca5ec4c6264891492df3e9ce91e
Ryota Higashi <ryo19870130@outlook.jp>
2022-01-11 04:05:03 +0900

    create Header

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Hello from './src/components/Hello';
@@ -6,10 +6,49 @@ import Hello from './src/components/Hello';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang>world</Hello>
-      <Hello bang style={{ fontSize: 16}}>small world</Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>Memo App</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2020年11月11日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2020年11月11日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2020年11月11日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      
+      <View>
+        <Text>X</Text>
+      </View>
+
     </View>
   );
 }
@@ -18,7 +57,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#0E2BC2',
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
+    backgroundColor: "gray",
+    alignItems: "center",
+  },
+  appbarRight: {
+    position: "absolute",
+    right: 19,
+    bottom: 18,
+    color: 'rgba(255, 255, 255, 0.8)',
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 24,
+    lineHeight: 32,
+    color: "#ffffff",
+    fontWeight: "bold",
   },
 });
