e834cadf04f6c23393da2bd924cadd2acd7ac41b
Ryota Higashi <ryo19870130@outlook.jp>
2022-01-21 21:56:40 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 
 // import MemoListScreen from './src/screens/MemoListScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
